6b533d060ac1257e880a220353620e84cf3f2385
ArthurAizawa <139519439+ArthurAizawa@users.noreply.github.com>
2025-04-02 22:34:59 -0300

    Desing resposive Update

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -8,29 +8,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
-	
+
 	private static Scene mainScene;
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			 Parent parent = loader.load();
-			 mainScene = new Scene(parent);
-			 primaryStage.setScene(mainScene);
-			 primaryStage.setTitle("Sample JavaFX application");
-			 primaryStage.show();
-			 } catch (IOException e) {
-			 e.printStackTrace();
-			 }
+			Parent parent = loader.load();
+			mainScene = new Scene(parent);
+			mainScene.getStylesheets().clear();
+			mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public static Scene getMainScene() {
 		return mainScene;
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
